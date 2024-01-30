bbb543d2fd499663535a043b10ca02d0cacec9b8
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-01-29 23:44:48 -0500

    added todo

--- nba_game_time/nba_game_time/google_calendar_integration.py
+++ nba_game_time/nba_game_time/google_calendar_integration.py
@@ -69,6 +69,9 @@ def create_events(response):
 
 
 if __name__ == '__main__':
+    # To do:
+    # Remove the old calendar
+
     # Create a calendar
     response = create_calendar()
     # Create events
