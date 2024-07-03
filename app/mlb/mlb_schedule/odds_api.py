af874c5ff0d8df8daf578c0b927b4c0c57a8ab6c
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-07-02 22:50:17 -0400

    Implemented odds movement link for main page

--- app/mlb/mlb_schedule/odds_api.py
+++ app/mlb/mlb_schedule/odds_api.py
@@ -1,12 +1,22 @@
 import requests
+from datetime import datetime, timedelta
 from .config import API_KEY, SPORT, REGIONS, MARKETS, ODDS_FORMAT, DATE_FORMAT
 
 
 def fetch_events():
+    # Get today's date in ISO 8601 format
+    today = datetime.utcnow()
+    tomorrow = today + timedelta(days=1)
+    today_str = today.strftime('%Y-%m-%dT00:00:00Z')
+    tomorrow_str = tomorrow.strftime('%Y-%m-%dT00:00:00Z')
+
     response = requests.get(
         f'https://api.the-odds-api.com/v4/sports/{SPORT}/events',
         params={
             'apiKey' : API_KEY,
+            'commenceTimeFrom': today_str,
+            'commenceTimeTo': tomorrow_str,
+            'dateFormat': 'iso'
         }
     )
     if response.status_code != 200:
