4bab021e7f7e53ced56d05a3e1f1b2589c6367a8
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-07-08 09:47:20 -0400

    fixed get schedule bug

--- app/mlb/mlb_schedule/schedule_api.py
+++ app/mlb/mlb_schedule/schedule_api.py
@@ -1,7 +1,21 @@
 import requests
+from datetime import datetime, timezone, timedelta
+import pytz
 
 class MLBAPI:
     BASE_URL = 'https://statsapi.mlb.com/api/v1/'
+
+    @staticmethod
+    def get_current_est_date():
+        """
+        Get the current date in EST.
+
+        Returns:
+            str: The current date in EST in YYYY-MM-DD format.
+        """
+        est = pytz.timezone('US/Eastern')
+        now_est = datetime.now(est)
+        return now_est.strftime('%Y-%m-%d')
 
     @staticmethod
     def get_schedule(sport_id=1):
@@ -17,7 +31,8 @@ class MLBAPI:
         Raises:
             requests.exceptions.RequestException: If the request fails or returns a bad status code.
         """
-        url = f"{MLBAPI.BASE_URL}/schedule/games/?sportId={sport_id}"
+        current_date = MLBAPI.get_current_est_date()
+        url = f"{MLBAPI.BASE_URL}/schedule/games/?sportId={sport_id}&startDate={current_date}&endDate={current_date}"
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for bad status codes
         return response.json()
