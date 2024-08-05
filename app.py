2324f783a7cc5f10f948f64951ff9ed712534078
tim506877 <tim506877@gmail.com>
2024-08-05 16:00:54 -0400

    Fix path

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-from app.mlb.mlb_schedule.routes import mlb_schedule_bp
-
-
-def create_app():
-    app = Flask(__name__)
-
-    app.register_blueprint(mlb_schedule_bp, url_prefix='/mlb')
-
-    return app
+from app import create_app
 
 app = create_app()
 
