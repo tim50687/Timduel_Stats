c4122b4efa9107cc6ee0e0a2020f846ce893ea00
tim506877 <tim506877@gmail.com>
2024-09-22 15:41:30 -0400

    Modified url prefix for web app

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from api.mlb.routes import mlb_schedule_bp
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(mlb_schedule_bp, url_prefix='/')
+    app.register_blueprint(mlb_schedule_bp, url_prefix='/mlb')
 
     return app
 
