2857572a469f93cb45b2b752305afac0706e940c
tim506877 <tim506877@gmail.com>
2024-09-22 15:50:18 -0400

    Add home page to web app

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ print(project_root)
 
 from flask import Flask
 from api.mlb.routes import mlb_schedule_bp
-
+from api.home_routes import home_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Home page
+    app.register_blueprint(home_bp, url_prefix='/')
 
     app.register_blueprint(mlb_schedule_bp, url_prefix='/mlb')
 
