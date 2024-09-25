bf0a838aa1b190e77fb0d10a25527a98841d695c
tim506877 <tim506877@gmail.com>
2024-09-25 17:32:55 -0400

    Add tailwind CSS to schedule page

---
+++ static/mlb/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../../api/mlb/templates/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  safelist: ["text-4xl", "text-5xl"],
+  plugins: [],
+};
